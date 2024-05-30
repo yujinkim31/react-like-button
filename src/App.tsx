ef393776d7857bd2dad15da72e4f2fb3c6c799a2
Yujin Kim <qnqn0828@naver.com>
2024-05-30 15:06:07 +0900

    좋아요 좋아요취소버튼만들기 삼항연산자버전

--- src/App.tsx
+++ src/App.tsx
@@ -4,40 +4,20 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  // const [count, setCount] = useState(0);
   const [like, setLike] = useState(false);
-  const like반전 = () => setLike((현재like) => !현재like);
-  let likeButton = <button onClick={like반전}>좋아요버튼</button>;
-  if (like === true) {
-    likeButton = <button onClick={like반전}>좋아요취소버튼</button>;
-  }
-  return (
-    <div>
-      <h1>좋아요버튼만들기</h1>
-      {likeButton}
-    </div>
+  const 클릭시_실행함수 = () => setLike((prev) => !prev);
+
+  const 좋아요또는취소버튼 = like ? (
+    <button onClick={클릭시_실행함수}>좋아요취소</button>
+  ) : (
+    <button onClick={클릭시_실행함수}>좋아요</button>
   );
 
-  {
-    /* // <div>
-    //   like === false ? {<button
-    //     onClick={() => {
-    //       setLike((like) => false);
-    //     }}
-    //   >
-    //     좋아요버튼
-    //   </button>} : {<button
-    //     onClick={() => {
-    //       setLike((like) => true);
-    //     }}
-    //   >
-    //     좋아요취소버튼
-    //   </button>}
-      
-
-    //   <button>싫어요버튼</button>
-    // </div> */
-  }
+  return (
+    <div>
+      <h1>{좋아요또는취소버튼}</h1>
+    </div>
+  );
 }
 
 export default App;
