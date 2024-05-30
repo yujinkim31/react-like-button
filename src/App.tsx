0b5d2b87d5bda0e46bd098b31bbe775d9cbc5e66
Yujin Kim <qnqn0828@naver.com>
2024-05-30 14:50:51 +0900

    좋아요 좋아요 취소 버튼 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,43 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  
-  return <div>
-    <button>좋아요버튼</button>
-    <button>싫어요버튼</button>
-  </div>;
+  // const [count, setCount] = useState(0);
+  const [like, setLike] = useState(false);
+  const like반전 = () => setLike((현재like) => !현재like);
+  let likeButton = <button onClick={like반전}>좋아요버튼</button>;
+  if (like === true) {
+    likeButton = <button onClick={like반전}>좋아요취소버튼</button>;
+  }
+  return (
+    <div>
+      <h1>좋아요버튼만들기</h1>
+      {likeButton}
+    </div>
+  );
+
+  {
+    /* // <div>
+    //   like === false ? {<button
+    //     onClick={() => {
+    //       setLike((like) => false);
+    //     }}
+    //   >
+    //     좋아요버튼
+    //   </button>} : {<button
+    //     onClick={() => {
+    //       setLike((like) => true);
+    //     }}
+    //   >
+    //     좋아요취소버튼
+    //   </button>}
+      
+
+    //   <button>싫어요버튼</button>
+    // </div> */
+  }
 }
 
 export default App;
